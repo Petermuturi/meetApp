3d38ad71fd3f552b873614a4074ad9603834d2f3
Petermuturi <pmuturi65@gmail.com>
2015-11-09 13:22:48 +0300

    new scripts

--- js/upload.js
+++ js/upload.js
@@ -9,9 +9,8 @@ $(document).ready(function() {
                 $('.profile-pic').attr('src', e.target.result);
             };
           reader.onload = function (e) {
-                $('.photo1').attr('src', e.target.result);
+                $('.photox').attr('src', e.target.result);
             };
-          
                       reader.readAsDataURL(input.files[0]);
         }
     };
@@ -23,5 +22,110 @@ $(document).ready(function() {
     
     $(".upload-button").on('click', function() {
        $(".file-upload").click();
+    });
+});
+// bar pic
+$(document).ready(function() {    
+    var readURL = function(input) {
+        if (input.files && input.files[0]) {
+            var reader = new FileReader();
+
+         reader.onload = function (e) {
+                $('.photobar').attr('src', e.target.result);
+            };
+          
+                      reader.readAsDataURL(input.files[0]);
+        }
+    };
+    $(".file-uploadbar").on('change', function(){
+        readURL(this);
+    });
+    
+    $(".upload-buttonbar").on('click', function() {
+       $(".file-uploadbar").click();
+    });
+});
+// nightlife pic
+$(document).ready(function() {    
+    var readURL = function(input) {
+        if (input.files && input.files[0]) {
+            var reader = new FileReader();
+
+         reader.onload = function (e) {
+                $('.photolife').attr('src', e.target.result);
+            };
+          
+                      reader.readAsDataURL(input.files[0]);
+        }
+    };
+    $(".file-uploadlife").on('change', function(){
+        readURL(this);
+    });
+    
+    $(".upload-buttonlife").on('click', function() {
+       $(".file-uploadlife").click();
+    });
+});
+// shopping pic
+$(document).ready(function() {    
+    var readURL = function(input) {
+        if (input.files && input.files[0]) {
+            var reader = new FileReader();
+
+         reader.onload = function (e) {
+                $('.photoshop').attr('src', e.target.result);
+            };
+          
+                      reader.readAsDataURL(input.files[0]);
+        }
+    };
+    $(".file-uploadshop").on('change', function(){
+        readURL(this);
+    });
+    
+    $(".upload-buttonshop").on('click', function() {
+       $(".file-uploadshop").click();
+    });
+});
+// activity pic
+$(document).ready(function() {    
+    var readURL = function(input) {
+        if (input.files && input.files[0]) {
+            var reader = new FileReader();
+
+         reader.onload = function (e) {
+                $('.photoactivity').attr('src', e.target.result);
+            };
+          
+                      reader.readAsDataURL(input.files[0]);
+        }
+    };
+    $(".file-uploadactivity").on('change', function(){
+        readURL(this);
+    });
+    
+    $(".upload-buttonactivity").on('click', function() {
+       $(".file-uploadactivity").click();
+    });
+});
+// event pic
+$(document).ready(function() {    
+    var readURL = function(input) {
+        if (input.files && input.files[0]) {
+            var reader = new FileReader();
+
+         reader.onload = function (e) {
+                $('.photoevent').attr('src', e.target.result);
+            };
+          
+                      reader.readAsDataURL(input.files[0]);
+        }
+    };
+    $(".file-uploadevent").on('change', function(){
+        readURL(this);
+    });
+    
+    $(".upload-buttonevent").on('click', function() {
+       $(".file-uploadevent").click();
     });
 });
